16bf0eb9ff03bd832c2ad671befe55727e63f792
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-05-26 10:11:41 -0400

    [Clase 3] Instalacion del entorno de desarrollo

---
+++ [0] Codigos/[Clase 3] Hola mundo/main.js
@@ -0,0 +1,8 @@
+// Primer comando, el mas comun, es el console.log
+console.log("Hola mundo");
+// Para correr este comando se necesita abrir la terminal y escribir:
+// node main.js
+
+// Siempre y cuando estemos en la carpeta donde se encuentra el archivo main.js
+// En este caso deberiamos estar en Fundamentos-JS/[0] Codigos/[Clase 3] Hola mundo
+// Si no estamos en la carpeta correcta, podemos navegar a ella usando el comando cd
