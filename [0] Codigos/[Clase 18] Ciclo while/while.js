491f92c623392fec8057cf36f041d3e1f6de3553
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-06-06 14:42:34 -0400

    [Clase 18] Ciclo while

---
+++ [0] Codigos/[Clase 18] Ciclo while/while.js
@@ -0,0 +1,15 @@
+// El ciclo while se utiliza para ejecutar un bloque de código mientras una condición sea verdadera.
+
+// La estructura es la siguiente:
+
+// while (condicion){
+//codigo
+// }
+
+// Un ejemplo sencillo de un ciclo while que imprime números del 1 al 10:
+
+let contador = 1;
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
